d0e33a3aaf64482b2521edfcb8933052a21968b1
duanqg <duanqigeng@gmail.com>
2020-09-11 12:52:00 +0800

    工程化

---
+++ src/mecator.py
@@ -0,0 +1,5 @@
+import gdal
+import os
+
+# os.system("gdalwarp -tr 0.00001 -0.00001 '/Users/duanqigeng/Desktop/gis_data/test.tif' '/Users/duanqigeng/Desktop/gis_data/out.tif'")
+# os.system("gdal2tiles.py -r bilinear -z 9 '/Users/duanqigeng/Desktop/gis_data/sichuan.vrt' '/Users/duanqigeng/Desktop/gis_data/tile'")
--- src/multfy.py
+++ src/multfy.py
@@ -1,39 +1,43 @@
 import os
 from PIL import Image
 import PIL.ImageChops as IC
-import numpy as Np
+import PIL.ImageEnhance as IE #对比度
+import os
 import cv2
+
+# 输出山体阴影图
+# os.system("gdaldem hillshade -az 45 -z 1 'sichuan.tif' 'hillshade.tif'")
+
+# 输出坡度坡向
+# os.system("gdaldem slope 'sichuan.tif' 'sichuan_slopes.tif'")
+
+# 坡度坡向黑白反转
+# os.system("gdaldem color-relief 'sichuan_slopes.tif' 'color-slope.txt' 'sichuan_slope.tif'")
+
+# 删除坡度坡向
+# os.remove()
+
+# 输出山体晕染图
+# os.system("gdaldem hillshade -az 45 -z 1.3 'sichuan.tif' 'hillshade.tif'")
 
 # 读取slope和relief
 img_r = Image.open(r'/Users/duanqigeng/Desktop/gis_data/sichuan_relief.tif')
 img_s = Image.open(r'/Users/duanqigeng/Desktop/gis_data/sichuan_slope.tif')
+
+# 读取灰度山体阴影(灰度图)
+img_h = cv2.imread(r'/Users/duanqigeng/Desktop/gis_data/sichuan_hillshade.tif', cv2.IMREAD_GRAYSCALE)
+# 将灰度图转化为RGB
+img_h = cv2.cvtColor(img_h, cv2.COLOR_GRAY2RGB)
+
 # 第一次正片叠底输出rs_RGB
 img_rs = IC.multiply(img_r, img_s)
+# cv2转成pillow
+img_h = Image.fromarray(img_h)
+en = IE.Brightness(img_h)
+img_h = en.enhance(1.75)  # 变亮
 
-# 添加Alpha通道,输出rs_RGBA
-# img_rs = img_rs.convert("RGBA")
-
-# 输出rs
-img_rs.save(r'/Users/duanqigeng/Desktop/gis_data/rs.jpg')
-
-# 读取灰度山体阴影
-cv_h = cv2.imread(r'/Users/duanqigeng/Desktop/gis_data/sichuan_hillshade.tif', cv2.IMREAD_GRAYSCALE)
-# 将灰度图转化为RGB
-cv_h_RGB = cv2.cvtColor(cv_h, cv2.COLOR_GRAY2RGB)
-
-# 将RGB转化为RGBA
-# cv_h_RGBA = cv2.cvtColor(cv_h_RGB, cv2.COLOR_RGB2RGBA)
-
-# 设置透明度
-# r_h, g_h, b_h, a_h = cv2.split(cv_h_RGBA)
-# a_h[:, :] = 125
-# cv_h_RGBA = cv2.merge([r_h, g_h, b_h, a_h])
-
-# 输出带有透明度的山体阴影
-cv2.imwrite(r'/Users/duanqigeng/Desktop/gis_data/h.jpg', cv_h_RGB)
-
-# PIL读取带有透明度的山体阴影
-img_h = Image.open(r'/Users/duanqigeng/Desktop/gis_data/h.jpg')
 img_o = IC.multiply(img_rs, img_h)
-# img_o = img_o.convert('RGB')
 img_o.save(r'/Users/duanqigeng/Desktop/gis_data/output.jpg')
+
+# 删除山体阴影、坡度坡向、晕染图、dem原始数据
+# os.remove()
