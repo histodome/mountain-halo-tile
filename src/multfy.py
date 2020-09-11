30ae31f08eed62b02ea901910b5eedaea7e4ceaf
duanqg <duanqigeng@gmail.com>
2020-09-11 14:35:32 +0800

    调整输出

--- src/multfy.py
+++ src/multfy.py
@@ -6,7 +6,7 @@ import os
 import cv2
 
 # 输出山体阴影图
-# os.system("gdaldem hillshade -az 45 -z 1 'sichuan.tif' 'hillshade.tif'")
+# os.system("gdaldem hillshade -az 45 -z 1.5 'sichuan.tif' 'hillshade.tif'")
 
 # 输出坡度坡向
 # os.system("gdaldem slope 'sichuan.tif' 'sichuan_slopes.tif'")
@@ -21,11 +21,11 @@ import cv2
 # os.system("gdaldem hillshade -az 45 -z 1.3 'sichuan.tif' 'hillshade.tif'")
 
 # 读取slope和relief
-img_r = Image.open(r'/Users/duanqigeng/Desktop/gis_data/sichuan_relief.tif')
-img_s = Image.open(r'/Users/duanqigeng/Desktop/gis_data/sichuan_slope.tif')
+img_r = Image.open(r'/Users/duanqigeng/Desktop/gis_data/globle_relief.tif')
+img_s = Image.open(r'/Users/duanqigeng/Desktop/gis_data/globle_slope.tif')
 
 # 读取灰度山体阴影(灰度图)
-img_h = cv2.imread(r'/Users/duanqigeng/Desktop/gis_data/sichuan_hillshade.tif', cv2.IMREAD_GRAYSCALE)
+img_h = cv2.imread(r'/Users/duanqigeng/Desktop/gis_data/globle_hillshade.tif', cv2.IMREAD_GRAYSCALE)
 # 将灰度图转化为RGB
 img_h = cv2.cvtColor(img_h, cv2.COLOR_GRAY2RGB)
 
@@ -34,8 +34,7 @@ img_rs = IC.multiply(img_r, img_s)
 # cv2转成pillow
 img_h = Image.fromarray(img_h)
 en = IE.Brightness(img_h)
-img_h = en.enhance(1.75)  # 变亮
-
+img_h = en.enhance(1.5)  # 变亮
 img_o = IC.multiply(img_rs, img_h)
 img_o.save(r'/Users/duanqigeng/Desktop/gis_data/output.jpg')
 
